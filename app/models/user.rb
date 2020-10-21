70a09261c19d0c7e45d19a956dfd4e31fdab626a
Skywalkcat428 <k.inoue014@gmail.com>
2020-10-21 10:53:26 +0900

    ユーザー管理機能実装

---
+++ app/models/user.rb
@@ -0,0 +1,30 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates_format_of :email, { with: EMAIL_REGEX, uniqueness: { case_sensitive: false }}
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d{6,}]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+    validates :last_name
+    validates :first_name
+  end
+  
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+  
+  with_options presence: true
+    validates :nickname
+    validates :birth_date
+  end
+
+  has_many :purchases
+  has_many :items
+end
