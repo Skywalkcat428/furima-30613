d5ae7233e76efd700585882c97cc2a36b015e672
Skywalkcat428 <k.inoue014@gmail.com>
2020-10-23 10:10:32 +0900

    ActiveHash導入

---
+++ app/models/item.rb
@@ -0,0 +1,11 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category,
+
+  #空の投稿を保存できないようにする
+  validates :title, :text, :category, :condition, :delivery_charge, :prefecture, :delivery_date, presence: true
+
+  #ジャンルの選択が「--」の時は保存できないようにする
+  validates :category_id, numericality: { other_than: 1 } 
+  
+end
