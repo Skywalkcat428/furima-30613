3c03b84be88d1da95f48d4870ff3fa2419faff39
Skywalkcat428 <k.inoue014@gmail.com>
2020-10-26 21:49:20 +0900

    テストコード等修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -27,29 +27,29 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Detail can't be blank")
       end
       it 'カテゴリーが空だと登録できない' do
-        @item.category_id = '--'
+        @item.category_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Category is not a number')
+        expect(@item.errors.full_messages).to include('Category must be other than 1')
       end
       it '商品の状態が空だと登録できない' do
-        @item.condition_id = '--'
+        @item.condition_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Condition is not a number')
+        expect(@item.errors.full_messages).to include('Condition must be other than 1')
       end
       it '配送料の負担が空だと登録できない' do
-        @item.delivery_charge_id = '--'
+        @item.delivery_charge_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Delivery charge is not a number')
+        expect(@item.errors.full_messages).to include('Delivery charge must be other than 1')
       end
       it '発送元の地域が空だと登録できない' do
-        @item.prefecture_id = '--'
+        @item.prefecture_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Prefecture is not a number')
+        expect(@item.errors.full_messages).to include('Prefecture must be other than 1')
       end
       it '発送までの日数が空だと登録できない' do
-        @item.delivery_date_id = '--'
+        @item.delivery_date_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Delivery date is not a number')
+        expect(@item.errors.full_messages).to include('Delivery date must be other than 1')
       end
       it '価格が空だと登録できない' do
         @item.price = ''
