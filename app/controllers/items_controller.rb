8619edf646e274d5f82c9e3beb5cb7d9337b9fa7
Skywalkcat428 <k.inoue014@gmail.com>
2020-10-29 12:07:53 +0900

    条件分岐の順序を修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,8 +37,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.destroy
-      user_signed_in? && current_user == @item.user
+    if user_signed_in? && current_user == @item.user
+      @item.destroy
       return redirect_to root_path
     end
   end
