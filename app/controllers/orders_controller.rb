b5800f62c515f2f7a3419bdf3a300d9ed56daa8a
Skywalkcat428 <k.inoue014@gmail.com>
2020-11-03 15:36:05 +0900

    クレカ機能導入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,6 @@ class OrdersController < ApplicationController
   def index
     @order_address = OrderAddress.new
     @item = Item.find(params[:item_id])
-
     unless (user_signed_in? && current_user != @item.user) || '売却済み'
       redirect_to root_path
     end
@@ -12,8 +11,8 @@ class OrdersController < ApplicationController
   def create
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
-    # binding.pry
     if @order_address.valid?
+      pay_item
       @order_address.save
       return redirect_to root_path
     else
@@ -24,7 +23,16 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_address).permit(:post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.permit(:post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
   
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :order_id, :item_id, :user_id
+  attr_accessor :post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :order_id, :item_id, :user_id, :token
 
   with_options presence: true do
     validates :post_code,     format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
@@ -8,6 +8,7 @@ class OrderAddress
     validates :city
     validates :house_number
     validates :phone_number,  format: { with: /\A[0-9]+\z/ }
+    validates :token
   end
 
   def save
