7c98a4755a2b43d92ec18eaf3f254c53aea539ee
Skywalkcat428 <k.inoue014@gmail.com>
2020-11-04 17:15:00 +0900

    不要なファイルを削除

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -13,6 +13,6 @@ class OrderAddress
 
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
-    address = Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 end
